14305067a0edf364d861c867591b84a43cca1544
Khalid Sookia <khalidsookia@gmail.com>
2017-05-17 15:34:25 +0200

    udpated vagrant conf and installation bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,42 +15,41 @@ Cyan='\033[0;36m'         # Cyan
 #********************************************************************
 sudo locale-gen
 
+echo -e "$Cyan \n Add php apt repository $Color_Off"
+sudo add-apt-repository ppa:ondrej/php5-5.6 -y
+sudo apt-get install python-software-properties
+
 # Update packages and Upgrade system
 echo -e "$Cyan \n Updating System.. $Color_Off"
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-sudo apt-get install git vim
+echo -e "$Cyan \n MySQL Config $Color_Off"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password paris'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password paris'
+sudo apt-get install -y mysql-server
 
 ## Install LAMP
 echo -e "$Cyan \n Installing Apache2 and PHP $Color_Off"
+sudo apt-get install vim apache2 php5 libapache2-mod-php5 -y mysql-server
 
-sudo apt-get install software-properties-common
+sudo service apache2 restart
 
-sudo apt-add-repository -y ppa:ondrej/php 
-sudo apt-add-repository -y ppa:ondrej/mysql-5.5
-sudo apt-get -y update
+echo -e "$Cyan \n Installing PHP extensions $Color_Off"
+sudo apt-get install -y libcurl4-openssl-dev pkg-config libssl-dev libsslcommon2-dev curl php5-curl libcurl4-doc libcurl3-dbg libidn11-dev libkrb5-dev libldap2-dev librtmp-dev php5-mysql php5-mcrypt php5-cli php5-dev php5-common php5-gd php5-cgi
 
-sudo apt-get -y install php5.6-mysql php5.6-cli php5.6-curl php5.6-json php5.6-sqlite3 php5.6-mcrypt php5.6-curl 
-php-xdebug php5.6-mbstring libapache2-mod-php5.6 mysql-server php5-mysql
+sudo mysql_install_db
 
-sudo apt-get install apache2 libapache2-mod-php5
-
-sudo a2enmod php5.6 
-
-echo 2 | sudo update-alternatives --config php
-
-sudo apt-get update
-
-sudo add-apt-repository ppa:nijel/phpmyadmin;
-
-sudo apt-get update
-
-sudo apt-get install phpmyadmin
-
+Installing ruby for SASS and Compass support
+echo -e "$Cyan \n Installing ruby for SASS and Compass support $Color_Off"
+sudo apt-get install ruby-full -y
+sudo gem update -f
+sudo gem install compass
 
 echo -e "$Cyan \n NodeJS and npm install $Color_Off"
 sudo apt-get install nodejs npm -y
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+sudo npm install -g typescript gulp-cli concurrently typings grunt-cli bower
 
 #Install composer 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -60,19 +59,20 @@ php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
 
 echo -e "$Cyan \n Removing apache files $Color_Off"
-sudo rm /etc/php/5.6/apache2/php.ini /etc/php/5.6/cli/php.ini /etc/hosts
+# sudo rm /etc/php/7.0/apache2/php.ini /etc/php/7.0/cli/php.ini /etc/hosts
 
-echo -e "$Cyan \n Adding apache sites configuration $Color_Off"
-sudo cp /var/www/vagrant-stuffs/apache-conf/transmeo.conf /etc/apache2/sites-available/
+# echo -e "$Cyan \n Adding apache sites configuration $Color_Off"
+# sudo cp /var/www/vagrant-stuffs/apache-conf/pleaky.conf /etc/apache2/sites-available/
+# sudo cp /var/www/vagrant-stuffs/apache-conf/platform-pleaky.conf /etc/apache2/sites-available/
+# sudo cp /var/www/vagrant-stuffs/apache-conf/iknsa-idp.conf /etc/apache2/sites-available/
+# echo -e "$Cyan \n Adding apache config files php.ini and hosts $Color_Off"
+# sudo cp /var/www/vagrant-stuffs/apache-conf/cli-php.ini /etc/php/7.0/cli/php.ini
+# sudo cp /var/www/vagrant-stuffs/apache-conf/apache-php.ini /etc/php/7.0/apache2/php.ini
+# sudo cp /var/www/vagrant-stuffs/apache-conf/hosts /etc/hosts
 
-echo -e "$Cyan \n Adding apache config files php.ini and hosts $Color_Off"
-sudo cp /var/www/vagrant-stuffs/apache-conf/cli-php.ini /etc/php/5.6/cli/php.ini
-sudo cp /var/www/vagrant-stuffs/apache-conf/apache-php.ini /etc/php/5.6/apache2/php.ini
-sudo cp /var/www/vagrant-stuffs/apache-conf/hosts /etc/hosts
-
-echo -e "$Cyan \n Enabling apache sites $Color_Off"
-sudo a2ensite transmeo.conf
-sudo a2enmod rewrite
+# echo -e "$Cyan \n Enabling apache sites $Color_Off"
+# sudo a2ensite pleaky.conf platform-pleaky.conf iknsa-idp.conf
+# sudo a2enmod rewrite
 sudo service apache2 restart
 
 echo -e "$Cyan \n Adding rsa key to ssh agent $Color_Off"
@@ -85,14 +85,15 @@ chmod 400 ~/.ssh/id_rsa
 echo -e "$Cyan \n Add rsa key to ssh agent $Color_Off"
 ssh-add ~/.ssh/id_rsa
 
+sudo apt-get install git -y
+
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 
+cd /var/www/transmeo
+
+composer install
+bower install
 
 sudo service apache2 restart
 
-cd /var/www/transmeo
-composer update
-
-npm install 
-
-exit
+exit;
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-# if [ -d old_transmeo ]; then
-#     rm -rf old_transmeo
-# else
-#     if [[ -d transmeo ]]; then
-#         mv pleaky old_transmeo
-#     fi
-# fi
-git clone git@github.com:php-light/framework.git transmeo
+if [ -d old_transmeo ]; then
+    rm -rf old_transmeo
+else
+    if [[ -d transmeo ]]; then
+        mv transmeo old_transmeo
+    fi
+fi
+git clone git@github.com:iknsa-corp/cardif-transmeo.git transmeo
 
 cp ~/.ssh/id_rsa vagrant-stuffs
 
